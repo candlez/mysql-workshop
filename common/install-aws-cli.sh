1f5ddd096c57e56457b1621a9cb7d2820c64bde4
candlez <thomasmcnamara@vt.edu>
2025-05-24 13:38:37 -0400

    first draft of arm compatible image

--- common/install-aws-cli.sh
+++ common/install-aws-cli.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
 microdnf install -y unzip && microdnf clean all
-microdnf install -y cronie && microdnf clean all
-
-env | grep MYSQL_ROOT_PASSWORD > /etc/environment
-
-crond
-
+ 
 # Install AWS CLI if not already installed
 if ! command -v aws &> /dev/null; then
     echo "AWS CLI not found. Installing..."
     # here we will also install cron and start the daemon
-    curl -s "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip" && \
+    curl -s "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "/tmp/awscliv2.zip" && \
     unzip -qq /tmp/awscliv2.zip -d /tmp && \
     /tmp/aws/install && \
     rm -rf /tmp/awscliv2.zip /tmp/aws
--- main/custom-entrypoint.sh
+++ main/custom-entrypoint.sh
@@ -8,10 +8,5 @@ chmod +x /usr/local/bin/install-aws-cli.sh
 chmod +x /usr/local/bin/pull-backup.sh
 /usr/local/bin/pull-backup.sh
 
-# set up cron jobs for saving backups
-
-chmod +x /usr/local/bin/create-backup.sh
-echo "0 * * * * /usr/local/bin/create-backup.sh" | crontab -
-
 # now we go into normal set up of the mysql server
 exec /usr/local/bin/docker-entrypoint.sh mysqld
