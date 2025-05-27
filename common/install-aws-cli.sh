a7fa42dadfd9c0da45e4a1ba9af57df90d9d4c94
Thomas McNamara <thomasmcnamara@vt.edu>
2025-05-26 21:36:51 -0400

    fixed comments

--- common/install-aws-cli.sh
+++ common/install-aws-cli.sh
@@ -2,10 +2,9 @@
 
 microdnf install -y unzip && microdnf clean all
  
-# Install AWS CLI if not already installed
+# install AWS CLI if not already installed
 if ! command -v aws &> /dev/null; then
     echo "AWS CLI not found. Installing..."
-    # here we will also install cron and start the daemon
     curl -s "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "/tmp/awscliv2.zip" && \
     unzip -qq /tmp/awscliv2.zip -d /tmp && \
     /tmp/aws/install && \
